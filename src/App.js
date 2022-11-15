24e35e38f9f0faf6d154b1b40e5214d4305d3772
azz <azzshakhsheer0@gmail.com>
2022-11-15 22:26:27 +0200

    my first Changes

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import SignIn from './SignIn/index';
+import ForgotPassword from './ForgotPassword/index'
+
+function App() {
+  return (
+    <SignIn />
+   //<ForgotPassword />
+  );
+}
+
+export default App;
